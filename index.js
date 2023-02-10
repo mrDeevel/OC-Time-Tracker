153f398bb15159ca6f37c60167f49f614724b7f3
mrDeevel <67458950+mrDeevel@users.noreply.github.com>
2023-02-10 11:24:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+    ${.g-signin2).css("display","none");
+    $(.data).css("display","block");
     console.lod(profile);
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
