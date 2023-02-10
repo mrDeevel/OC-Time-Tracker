797f8e3bebb3fbca3f91671e1136be9009b7b2a1
mrDeevel <67458950+mrDeevel@users.noreply.github.com>
2023-02-09 23:05:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ function onSignIn(googleUser) {
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
     $("#image").attr('src', profile.getImageUrl());
-    $(".data").css("display","block");
-    $("g-signin2").css("display","none");
   }
 
   function signOut() {
